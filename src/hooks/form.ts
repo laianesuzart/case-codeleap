e916d61766e0a564952f546a27be59532f62637a
Laiane Suzart <laiane@pm.me>
2025-05-01 14:43:09 -0300

    add button, text input components and form hook integration

---
+++ src/hooks/form.ts
@@ -0,0 +1,20 @@
+import { createFormHook } from "@tanstack/react-form";
+
+import {
+	SubscribeButton,
+	TextArea,
+	TextField,
+} from "../components/FormComponents";
+import { fieldContext, formContext } from "./form-context";
+
+export const { useAppForm } = createFormHook({
+	fieldComponents: {
+		TextField,
+		TextArea,
+	},
+	formComponents: {
+		SubscribeButton,
+	},
+	fieldContext,
+	formContext,
+});
