cb7c377d0698f5699a58a27b404a7e92e48df303
Laiane Suzart <laiane@pm.me>
2025-05-02 01:42:29 -0300

    add modal component

---
+++ src/components/Modal/Content.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react";
+
+interface TextContent {
+	text: string;
+	children?: never;
+}
+
+interface NodeContent {
+	children: ReactNode;
+	text?: never;
+}
+
+type Props = TextContent | NodeContent;
+
+export function Content({ text, children }: Props) {
+	return <>{children ? children : <p className="leading-tight">{text}</p>}</>;
+}
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,9 @@
+import { Content } from "./Content";
+import { Root } from "./Root";
+import { Title } from "./Title";
+
+export const Modal = {
+	Root,
+	Title,
+	Content,
+};
